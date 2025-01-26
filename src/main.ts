218e304cbeaf1331960b76b0572758a0f3348a60
Taras Krasitskyi <taras.krasitsky@gmail.com>
2025-01-26 14:48:17 +0200

    added  ConfigService, TypeOrm

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('v1/api');
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 
